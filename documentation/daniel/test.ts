130bf9ab69cf48d2cebfb242eb145e897d686982
neildan <felipeluiz152000@gmail.com>
2022-08-11 14:57:21 -0500

    Ejercicio piramide

--- documentation/daniel/test.ts
+++ documentation/daniel/test.ts
@@ -1,15 +1,38 @@
 console.log("Archivo de Daniel Valencia")
 
-class Circunferencia{
-    public radio: number
-    constructor (RadioCircunferencia: number){
-        this.radio = RadioCircunferencia
-    }
-    getArea():Number{
-        return Math.round(this.radio * this.radio * Math.PI)
+class DrawFigures {
+    private size: number
+    private boardSize: number
+
+    constructor (size: number){
+        this.size = size + 1
+        this.boardSize = this.size * 2
     }
 
+    buildPyramid() {
+        for(let i: number = 0; i < this.size; i++) {
+            this.printNumber(i, this.boardSize - i)
+        }
+    }
+
+    printNumber(num: number, blankSpaces: number) {
+        let result: string = ''
+        result = this.printBlankSpaces(blankSpaces)
+        for(let i: number = 0; i < num; i++){
+            if(i !== 0) result += ' '
+            result += num
+        }
+        result += this.printBlankSpaces(blankSpaces)
+        console.log(result)
+    }
+
+    printBlankSpaces(num: number) {
+        let spaces: string = ''
+        for(let i: number = 0; i < num; i++){
+            spaces += ' '
+        }
+        return spaces
+    }
 }
 
-const Circulo = new Circunferencia(10)
-console.log(Circulo.getArea())
+new DrawFigures(9).buildPyramid() // max 9
