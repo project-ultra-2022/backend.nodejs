bc7f3b5ade952a3ce785c1e50aaef2fced78bf89
Lodhost7 <pipelodionx@gmail.com>
2022-08-12 17:08:04 -0500

    Tarea figuras

---
+++ documentation/pipe/Tareas_clase_5/Tarea_figuras.ts
@@ -0,0 +1,46 @@
+// RECTANGULO
+/*class rectangulo{ 
+    public alto: number 
+    public ancho: number
+    constructor (alto : number, ancho: number){
+    this.alto = alto 
+    this.ancho = ancho 
+    
+    }
+    areaRectangulo (){
+        return this.alto * this.ancho 
+    }
+}
+const area = new rectangulo(10,10)
+console.log(area)*/
+
+// TRIANGULO    
+/*class triangulo {
+    public alto : number 
+    public base : number 
+
+    constructor(alto : number, base : number){
+        this.alto = alto
+        this.base = base 
+    }
+    areaTriangulo (){
+        return (this.alto * this.base)/2
+    }
+}
+const area = new triangulo(10, 10)
+console.log(area)*/
+
+//CIRCULO 
+
+/*class circulo{
+    public radio : number 
+
+    constructor(radio : number){
+        this.radio = radio 
+    }
+    areaCirculo (){
+        return Math.round(this.radio * this.radio * Math.PI )
+    }
+}
+const area = new circulo (10)
+console.log(area.radio)*/
