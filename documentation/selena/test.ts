646b7fa70bd104ac94eaf3a3563b2b68879a09f6
victoria181 <angiemerchan0506@gmail.com>
2022-08-12 16:58:41 -0500

    tarea figuritas

--- documentation/selena/test.ts
+++ documentation/selena/test.ts
@@ -1,1 +1,15 @@
 console.log("Archivo de Selena Merchan")
+class Triangulo {
+    private altura: number
+    private base: number
+
+   constructor (altura: number, base: number){
+       this.altura = altura 
+       this.base =base
+   }
+   area() {
+       return (this.base* this.altura)/2
+   }
+}
+let triangulito = new Triangulo(10, 10)
+console.log(triangulito.area())
