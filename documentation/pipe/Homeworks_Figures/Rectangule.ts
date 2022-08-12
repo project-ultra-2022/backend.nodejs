b0585e4b8c71b315ac236972565acda779664e17
Lodhost7 <pipelodionx@gmail.com>
2022-08-12 17:52:08 -0500

    Figures/2

---
+++ documentation/pipe/Homeworks_Figures/Rectangule.ts
@@ -0,0 +1,17 @@
+// RECTANGULO
+
+class rectangulo{ 
+    public alto: number 
+    public ancho: number
+    constructor (alto : number, ancho: number){
+    this.alto = alto 
+    this.ancho = ancho 
+    
+    }
+    areaRectangulo (){
+        return this.alto * this.ancho 
+    }
+}
+const area = new rectangulo(10,10)
+console.log(area)
+
