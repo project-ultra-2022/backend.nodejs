c59a1c0ed7c13684e9b4f1597336b6f77cc09267
Lodhost7 <pipelodionx@gmail.com>
2022-08-21 21:31:18 -0500

    cambios

---
+++ documentation/pipe/Homeworks/Recursividad.ts
@@ -0,0 +1,18 @@
+/*let ArrayRange: Array<number> = [];
+function rangoHasta(indice: number, num1: number = 0): Array<number> {
+  ArrayRange.push(num1);
+  if (ArrayRange.length <= indice) rangoHasta(indice, num1 + 1);
+  return ArrayRange;
+}
+console.log(rangoHasta(7));
+*/
+
+/*
+let ArrayRange: Array<number> = [];
+function rangoHasta(indice: number, num1: number = 5): Array<number> {
+    ArrayRange.push(num1);
+    if (ArrayRange.length <= indice) rangoHasta(indice, num1 + 1);
+    return ArrayRange;
+  }
+  console.log(rangoHasta(5));
+  */
--- documentation/pipe/test.ts
+++ documentation/pipe/test.ts
@@ -1,1 +1,9 @@
-console.log('Archivo de Pipe')
+console.log("Archivo de Pipe");
+
+let ArrayRange: Array<number> = [];
+function rangoHasta(desde: number, hasta: number): Array<number> {
+  ArrayRange.push(desde);
+  if (ArrayRange.length <= desde) rangoHasta(desde, hasta + 1);
+  return ArrayRange;
+}
+console.log(rangoHasta(5, 10));
